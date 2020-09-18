4c47c19c5951cf4107d0ede307c56ce2242fcc67
JaredSantiag <jaredartueo105@gmail.com>
2020-09-17 22:33:45 -0500

    Primer commit

---
+++ aplicacion.py
@@ -0,0 +1,50 @@
+import aiml
+from flask import Flask, request
+from pymessenger.bot import Bot
+import os
+
+kernel = aiml.Kernel()
+kernel.learn("hola.xml")
+kernel.respond("cargar aiml b")
+
+app = Flask(__name__)
+TokenDeAcceso = 'EAAFRBKfMqiIBAOw8F3VhaC6upDwP7gmU9A6ni6GKEZByW0OiaRkKWUvbZA8c2Gb3zLo7eVdR4I24WWkxwzq4OWWnzcSDEGr2Sbpfyi7KgwhdP3ebQlSEkMMdLF7VckZAc52uj3Cln5mQ4mRLJEmuZCeyD5GjjEof1ETBUkVMbwZDZD'
+TokenDeVerificacion = 'tokenprueba' 
+bot = Bot(TokenDeAcceso)
+ 
+@app.route("/", methods=['GET', 'POST'])
+def recibirMensaje():
+    if request.method == 'GET':
+        tokenDeEnvio = request.args.get("hub.verify_token")
+        return tokenFacebook(tokenDeEnvio)
+    
+    else:
+       output = request.get_json()
+       for event in output['entry']:
+          mensajeria = event['messaging']
+          for mensaje in mensajeria:
+            if mensaje.get('message'):
+                remitenteID = mensaje['sender']['id']
+                if mensaje['message'].get('text'):
+                    mensaje = mensaje['message'].get('text')
+                    respuestatext = obtenerMensajeBot(mensaje)
+                    enviarMensaje(remitenteID, respuestatext)
+
+    return "Mensaje Procesado"
+
+
+def tokenFacebook(tokenDeEnvio):
+    if tokenDeEnvio == TokenDeVerificacion:
+        return request.args.get("hub.challenge")
+    return 'Token de Verificacion Invalido'
+
+def obtenerMensajeBot(mensaje1):
+    print (mensaje1)
+    return kernel.respond(mensaje1)
+
+def enviarMensaje(remitenteID, respuesta):
+    bot.send_text_message(remitenteID, respuesta)
+    return "Mensaje enviado con exito"
+
+if __name__ == "__main__":
+    app.run()
